66f53bb8b7ef1d5ee976fbc4160cb85d6c3b1afb
OseiSilasYaw <41008484+OseiSilasYaw@users.noreply.github.com>
2018-07-09 15:07:16 +0000

    Add files via upload

---
+++ dividend_divisor.py
@@ -0,0 +1,10 @@
+print('\t\tNew Here\n')
+dividend= eval(input ('Enter the dividend: '))
+
+divisor = eval(input ('Enter the divisor: '))
+
+if divisor !=0:
+    print(dividend/divisor)
+    
+else:
+    print('Dont be stupid')
